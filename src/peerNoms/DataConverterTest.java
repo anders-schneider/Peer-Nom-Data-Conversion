0347015fba329d0577503e64d88046ef3505ab62
anders-schneider <aschn@seas.upenn.edu>
2015-07-02 01:45:46 -0700

    Added first reflection unit test

--- src/peerNoms/DataConverterTest.java
+++ src/peerNoms/DataConverterTest.java
@@ -2,14 +2,18 @@ package peerNoms;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.*;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class DataConverterTest {
 
+	DataConverter dc;
+	
 	@Before
 	public void setUp() throws Exception {
-		DataConverter dc = new DataConverter();
+		dc = new DataConverter();
 	}
 
 	@Test
@@ -25,9 +29,26 @@ public class DataConverterTest {
 	}
 
 	@Test
-	public void testCreateColHeader() {
-		//TODO Test
-		fail("Not yet implemented");
+	public void testCreateColHeader() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Class[] cArg = new Class[1];
+		cArg[0] = String.class;
+		Method createColHeader = dc.getClass().getDeclaredMethod("createColHeader", cArg);
+		createColHeader.setAccessible(true);
+		
+		String s1 = "\"Team: 1776ers / In the past month, who did you spend time with from your team (this / could be time s...- Andrew Arcuicci-Group\"";
+		String result1 = (String) createColHeader.invoke(dc, s1);
+		
+		assertEquals("Andrew Arcuicci", result1);
+		
+		String s2 = "\"Team: Dragons / In the past month, who did you spend time with from your team (this / could be time s...- Anam  Josan-Group\"";
+		String result2 = (String) createColHeader.invoke(dc, s2);
+		
+		assertEquals("Anam  Josan", result2);
+		
+		String s3 = "\"Team: Dragons / In the past month, who did you spend time with from your team (this / could be time s...- Anam Middle Josan-Smith-Group\"";
+		String result3 = (String) createColHeader.invoke(dc, s3);
+		
+		assertEquals("Anam Middle Josan-Smith", result3);
 	}
 	
 	@Test
